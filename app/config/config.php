8619b467d3a52cbeba78928b360b18aa7011769a
8uns <bunsdev08@gmail.com>
2024-10-09 10:56:11 +0900

    update config.php local

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
-// define('BASEURL', 'http://localhost/deteksi_plagiarisme/public/');
-define('BASEURL', 'http://103.67.79.236/pasca_ilmu_hukum/public/');
+define('BASEURL', 'http://localhost/deteksi_plagiarisme/public/');
+// define('BASEURL', 'http://103.67.79.236/pasca_ilmu_hukum/public/');
 // // define('BASEURL', 'http://192.168.1.18:8080/deteksi_plagiarisme/public/');
 
 // // URL
@@ -10,13 +10,13 @@ define('BASEURL', 'http://103.67.79.236/pasca_ilmu_hukum/public/');
 //     . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME'])
 //     . (!str_ends_with(dirname($_SERVER['SCRIPT_NAME']), "/") ? "/" : ""));
 
-// // DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'pascailkumDB');
-define('DB_PASS', '#DBPascailkum!123');
-define('DB_NAME', 'plagiarism_detection');
-
+// // // DB
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
+// define('DB_USER', 'pascailkumDB');
+// define('DB_PASS', '#DBPascailkum!123');
 // define('DB_NAME', 'plagiarism_detection');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'plagiarism_detection');
